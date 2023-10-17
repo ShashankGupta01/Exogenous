431af72168aacc8e6d3b4faf3c042f8b03676feb
ShashankGupta01 <146845624+ShashankGupta01@users.noreply.github.com>
2023-10-17 23:03:00 +0530

    Update Time_Series_Models.R

--- Time_Series_Models.R
+++ Time_Series_Models.R
@@ -24,14 +24,14 @@ ARIMA_Exo_var = auto.arima(xreg_Train)
 
 predARIMA_Exo_var= forecast::forecast(ARIMA_Exo_var,h=l) 
 xreg_pred= predARIMA_Exo_var$mean
-predARIMAX_Fit = forecast::forecast(fitARIMAX,xreg=xreg_pred,h=l) 
+pred_ARIMAX = forecast::forecast(fitARIMAX,xreg=xreg_pred,h=l) 
 
 
 ## ARNNX
 fit_nnarx = nnetar(train, xreg = xreg_Train, repeats = 100)
 fit_nnarx
 
-forecast_nnarx = forecast::forecast(fit_nnarx, h = l, xreg=xreg_pred) 
+pred_nnarx = forecast::forecast(fit_nnarx, h = l, xreg=xreg_pred) 
 
 
 ## ARIMAX + ARNNX
@@ -39,17 +39,17 @@ fit_res_ARNNX=nnetar(fitARIMAX$residuals,xreg=xreg_Train, repeats = 100)
 fit_res_ARNNX
 
 pred_res_ARNNX = forecast::forecast(fit_res_ARNNX, h=l, xreg=xreg_pred) 
-pred_arimax_arnnx=predARIMAX_Fit$mean+pred_res_ARNNX$mean
+pred_arimax_arnnx=pred_ARIMAX$mean+pred_res_ARNNX$mean
 
 
 ## ARIMAX + ARNN 
 fit_res_ARNN=nnetar(fitARIMAX$residuals, repeats = 100)
 fit_res_ARNN
 pred_res_ARNN = forecast::forecast(fit_res_ARNN, h=l) 
-pred_arimax_arnn=predARIMAX_Fit$mean+pred_res_ARNN$mean
+pred_arimax_arnn=pred_ARIMAX$mean+pred_res_ARNN$mean
 
 
 ##ETSX
 fitETSX = es(train, model = "ZZZ", xreg = xreg_Train)
 modelType(fitETSX)
-predETSX = forecast::forecast(fitETSX, h=l, xreg = xreg_Test)
+pred_ETSX = forecast::forecast(fitETSX, h=l, xreg = xreg_Test)
